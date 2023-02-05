fe20b5ee54571290ccdf3a20d23a2730e7bf084d
bilal.alnaani@fh-erfurt.de <bilal.alnaani@fh-erfurt.de>
2023-02-05 17:02:25 +0100

    add style to message content

--- apps/frontend/src/app/message/message.module.ts
+++ apps/frontend/src/app/message/message.module.ts
@@ -4,9 +4,10 @@ import { MessageBaseComponent } from './message-base/message-base.component';
 import { NgxGraphComponent } from '../mindmap/ngx-graph/ngx-graph.component';
 import { NgxGraphModule } from '@swimlane/ngx-graph';
 import { MessageComponent } from './message/message.component';
+import {AngularMaterialModule} from "../angular-material.module";
 
 @NgModule({
   declarations: [MessageBaseComponent, NgxGraphComponent, MessageComponent],
-  imports: [CommonModule, NgxGraphModule],
+  imports: [CommonModule, NgxGraphModule, AngularMaterialModule],
 })
 export class MessageModule {}
